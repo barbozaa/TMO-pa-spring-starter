bcf8e1e7d19d177a7fe205625eb50fe1fb912692
ABarboz4 <armando.barbozaojeda4@t-mobile.com>
2021-07-27 17:39:14 -0400

    Services created

---
+++ src/main/java/com/galvanize/tmo/paspringstarter/Books.java
@@ -0,0 +1,53 @@
+package com.galvanize.tmo.paspringstarter;
+
+import jdk.nashorn.internal.ir.annotations.Ignore;
+
+public class Books {
+    @Ignore
+    Integer id;
+    String title;
+    String author;
+    Integer datePublished;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Integer getDatePublished() {
+        return datePublished;
+    }
+
+    public void setDatePublished(Integer datePublished) {
+        this.datePublished = datePublished;
+    }
+
+    @Override
+    public String toString() {
+        return "Books{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", datePublished=" + datePublished +
+                '}';
+    }
+}
--- src/main/java/com/galvanize/tmo/paspringstarter/LibraryController.java
+++ src/main/java/com/galvanize/tmo/paspringstarter/LibraryController.java
@@ -1,13 +1,40 @@
 package com.galvanize.tmo.paspringstarter;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
 
 @RestController
 public class LibraryController {
+    List booksCache = new ArrayList<Books>();
 
     @GetMapping("/health")
     public void health() {
 
     }
+
+    @RequestMapping(value = "/api/books", method = RequestMethod.POST)
+    public Books getBooks(@RequestBody Books book) {
+        if(StringUtils.hasLength(book.getAuthor()) && book.getDatePublished() > 0){
+            book.setId(booksCache.size() + 1);
+            booksCache.add(book);
+            return book;
+        }
+        return new Books();
+    }
+
+    @GetMapping("/api/books")
+    public List getBooksSorted(){
+        Collections.sort(booksCache, Comparator.comparing(Books::getTitle));
+        return booksCache;
+    }
+
+        @DeleteMapping("/api/books")
+        public ResponseEntity<Void> deleteAllBooks(){
+            booksCache.clear();
+            return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
+        }
 }
